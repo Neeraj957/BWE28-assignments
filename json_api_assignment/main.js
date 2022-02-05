7c2bc6901482588daad9050853dce0e61b31c232
Neeraj957 <neerajnani.14@gmail.com>
2022-02-05 15:54:51 +0530

    Survey Form Assignment

--- json_api_assignment/main.js
+++ json_api_assignment/main.js
@@ -1,5 +1,5 @@
 var obj1 = { name: "Person 1", age:5 };
-var obj2 = { age:5, name: "Person 1" };
+var obj2 = { age:5, name: "Person 2" };
 
 var flag=true;
 
---
+++ json_api_assignment/restcountries.js
@@ -0,0 +1,4 @@
+
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(data => console.log(data));
